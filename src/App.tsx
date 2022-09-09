0113d5d130c0b6d11fd39fd0abd44e1a876972e7
thuclanarema <thuclan.arena@gmail.com>
2022-09-09 11:09:22 +0700

    create react app and setup eslint and prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import { AuthenticationPage } from './pages/RegisterPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<AuthenticationPage />} />
+            <Route
+                path="conversations"
+                element={(
+                    <div>
+                        <div>Conversations</div>
+                        <Outlet />
+                    </div>
+                )}
+            >
+                <Route path=":id" element={<div>Conversation ID Page</div>} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
